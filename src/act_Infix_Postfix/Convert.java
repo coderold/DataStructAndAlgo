8aa1a6162ea4f8e07b34f2207a62f5e74cacce2d
coderold <92320371+coderold@users.noreply.github.com>
2024-10-08 23:13:54 +0800

    tulog muna po ako

--- src/act_Infix_Postfix/Convert.java
+++ src/act_Infix_Postfix/Convert.java
@@ -1,9 +1,28 @@
 package act_Infix_Postfix;
 
+import java.util.Stack;
+
 public class Convert {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Stack<Character> stack = new Stack<>();
+		Stack<Character> postfix = new Stack<>();
+		
+		String eq = "5*(6+7)";
+		char[] token = eq.toCharArray();
+		
+		for(int i = 0; i < eq.length(); i++) {
+			if(Evaluate.isDigit(token[i])) {
+				postfix.push(token[i]);
+			}
+//			else if(token[i] == '') {
+//				
+//			}
+		}
+		
+		//tulog muna po ako pls
+		
 
 	}
 
